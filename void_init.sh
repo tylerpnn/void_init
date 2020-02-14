481ecea11c205450fb6ebd1c1607cc65d945f49a
Tyler Pennington <tylerpnn@gmail.com>
2020-02-14 02:13:51 -0800

    make more generic

--- void_init.sh
+++ void_init.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 # VOID SETUP SCRIPT
-HOME=/home/tyler
+WM=$1
 
 # Install base packages
-sudo xbps-install -Sy alsa-utils base-devel chromium cifs-utils cmake curl feh ffmpeg font-symbola fonts-nanum-ttf fonts-nanum-ttf-extra gimp git htop inxi ipafont-fonts-otf libX11-devel libXft-devel libXinerama-devel lm_sensors maim neofetch neovim plex-media-player ranger tmux xclip xorg zsh wget xcompmgr xorg-fonts xwallpaper mpv dmenu
-
-if [ "$1" = "--laptop" ]; then
-	sudo xbps-install -Sy acpi xf86-video-intel
-fi
+sudo xbps-install -Sy alsa-utils base-devel chromium cifs-utils cmake curl feh ffmpeg font-symbola fonts-nanum-ttf fonts-nanum-ttf-extra gimp git htop inxi ipafont-fonts-otf libX11-devel libXft-devel libXinerama-devel lm_sensors maim neofetch neovim plex-media-player ranger tmux xclip xorg zsh wget xcompmgr xorg-fonts xwallpaper mpv dmenu acpi
 
 mkdir -p $HOME/git
 cd $HOME/git
@@ -29,19 +25,12 @@ cd ..
 
 # Clone dotfiles and copy the essential ones
 git clone https://github.com/tylerpnn/dotfiles_void
-mkdir -p $HOME/.config
-mkdir -p $HOME/.local
 cd ./dotfiles_void
-cp -r ./.local/scripts $HOME/.local
-cp -r ./.local/share $HOME/.local
-cp -r ./.config/nvim $HOME/.config
-cp -r ./.config/zsh $HOME/.config
-cp ./.config/wall.png $HOME/.config
-cp ./.xinitrc $HOME/.xinitrc
-cp ./.bashrc $HOME/.bashrc
-cp ./.xprofile $HOME/.xprofile
-cp ./.profile $HOME/.profile
+git fetch origin $WM
+git checkout $WM
+cp -rf ./* $HOME/
 cd $HOME
+rm -rf .git
 rm .zprofile .bash_profile
 ln -s .profile .zprofile
 ln -s .profile .bash_profile
@@ -57,3 +46,10 @@ sudo cp ./Discord/discord.desktop /usr/share/applications
 sudo cp ./Discord/discord.png /usr/share/pixmaps
 sudo mv ./Discord /usr/share/discord
 sudo ln -s /usr/share/discord/Discord /usr/bin/Discord
+
+# Install iwd
+sudo xbps-install -Sy iwd
+sudo ln -s /etc/sv/iwd /var/service
+
+# Finally, change shell to zsh
+chsh -s `which zsh` $USER
