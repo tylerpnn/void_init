acaf605d2ed1c006c72d395291e6a12c734e6a1f
Tyler Pennington <tylerpnn@gmail.com>
2020-02-14 17:46:51 -0800

    enable kde5 install

--- void_init.sh
+++ void_init.sh
@@ -4,7 +4,7 @@
 WM=$1
 
 # Install base packages
-sudo xbps-install -Sy alsa-utils base-devel chromium cifs-utils cmake curl feh ffmpeg font-symbola fonts-nanum-ttf fonts-nanum-ttf-extra gimp git htop inxi ipafont-fonts-otf libX11-devel libXft-devel libXinerama-devel lm_sensors maim neofetch neovim plex-media-player ranger tmux xclip xorg zsh wget xcompmgr xorg-fonts xwallpaper mpv dmenu acpi
+sudo xbps-install -Sy acpi alsa-firmware alsa-tools alsa-utils base-devel chromium cifs-utils cmake curl dbus-glib dmenu feh ffmpeg font-symbola fonts-nanum-ttf fonts-nanum-ttf-extra gimp git htop inxi ipafont-fonts-otf libX11-devel libXft-devel libXinerama-devel lm_sensors maim mpv neofetch neovim plex-media-player ranger tmux wget xclip xcompmgr xorg xwallpaper zsh
 
 mkdir -p $HOME/git
 cd $HOME/git
@@ -23,6 +23,19 @@ if [ "$WM" = "dwm" ]; then
 	make
 	sudo make install
 	cd ..
+
+	# Install iwd
+	sudo xbps-install -Sy iwd dbus
+	sudo ln -s /etc/sv/dbus /var/service
+	sudo ln -s /etc/sv/iwd /var/service
+fi
+
+if [ "$WM" = "kde5" ]; then
+	sudo xbps-install -Sy kde5 dolphin udisks2 xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk NetworkManager network-manager-applet
+	sudo ln -s /etc/sv/dbus /var/service
+	sudo ln -s /etc/sv/sddm /var/service
+	sudo ln -s /etc/sv/NetworkManager /var/service
+	sudo usermod -a -G storage $USER
 fi
 
 # Clone dotfiles and copy the essential ones
@@ -48,11 +61,6 @@ sudo cp ./Discord/discord.desktop /usr/share/applications
 sudo cp ./Discord/discord.png /usr/share/pixmaps
 sudo mv ./Discord /usr/share/discord
 sudo ln -s /usr/share/discord/Discord /usr/bin/Discord
-
-# Install iwd
-sudo xbps-install -Sy iwd dbus
-sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/iwd /var/service
 
 # Finally, change shell to zsh
 chsh -s `which zsh` $USER
